7030a4a9e1249c249ad58cebbafc105a6091447d
Tom Tobar <codetombomb@gmail.com>
2020-09-05 13:42:45 -0500

    Added main

---
+++ src/main/main.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class main extends Component {
+    render() {
+        return (
+            <div>Hello from main</div>
+        )
+    }
+}
+export default main;
